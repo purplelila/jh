cfa504e7739837ba8cef8b8e71f74c1d61b99a6a
purplelila <110869955+purplelila@users.noreply.github.com>
2025-03-13 17:14:03 +0900

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,9 +0,0 @@
-let Footer = () => {
-    return (
-      <footer className="footer">
-        <p>© 2025 My Cafe App. All rights reserved.</p>
-      </footer>
-    );
-  };
-
-export default Footer;
