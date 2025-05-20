c1533d40480b30add6b5859c9ac94e8b70f7ef53
purplelila <110869955+purplelila@users.noreply.github.com>
2025-05-20 09:37:38 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Spring Boot 서버
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
